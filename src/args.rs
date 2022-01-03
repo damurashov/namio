47707102bef1ae2939f0d65f52fee9c424db9569
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-03 13:55:20 +0300

    [args] Impl | Comparison trait, regex strings

--- src/args.rs
+++ src/args.rs
@@ -1,18 +1,45 @@
-use std::{borrow::Cow};
+use core::cmp::PartialEq;
+use crate::name;
 
-struct Argpair(Cow<'static, str>, Cow<'static, str>);
+pub struct Args<'a>(&'a str, &'a str, &'a str, &'a str);
 
-impl Argpair {
-	fn long(&self) -> &str {
+impl<'a> Args<'a> {
+	fn as_long_primary(&'a self) -> & str {
 		&self.0
 	}
 
-	fn short(&self) -> &str {
+	fn as_short_primary(&self) -> &str {
 		&self.1
+	}
+
+	fn as_long_modified(&self) -> &str {
+		&self.2
+	}
+
+	fn as_short_modified(&self) -> &str {
+		&self.3
 	}
 }
 
-const APPEND_LABEL: Argpair = Argpair(Cow::Borrowed("--label"), Cow::Borrowed("-l"));
-const SET_LABEL: Argpair = Argpair(Cow::Borrowed("--Label"), Cow::Borrowed("-L"));
-const APPEND_YEAR: Argpair = Argpair(Cow::Borrowed("--year"), Cow::Borrowed("-y"));
-const SET_YEAR: Argpair = Argpair(Cow::Borrowed("--Year"), Cow::Borrowed("-Y"));
+impl<'a> PartialEq for Args<'a> {
+	fn eq(&self, other: &Self) -> bool {
+		self.0 == other.0 && self.1 == other.1 && self.2 == other.2 && self.3 == other.3
+	}
+}
+
+impl<'a> PartialEq<&str> for Args<'a> {
+	fn eq(&self, other: &&str) -> bool {
+		&self.0 == other || &self.1 == other || &self.2 == other || &self.3 == other
+	}
+}
+
+pub static YEAR: Args = Args("--year", "-y", "--Year", "-Y");
+pub static LABEL: Args = Args("--label", "-l", "--Label", "-L");
+pub static DATE: Args = Args("--date", "-d", "", "");
+
+pub mod re {
+	const YEAR: &'static str = r"[0-9]{4}";
+	const LABEL: &'static str = r"[A-Z]{2,}";
+	const WORD: &'static str = r"[0-9a-zA-Z]+";
+	const SEPARATOR: &'static str = r"[_-\s]";
+}
